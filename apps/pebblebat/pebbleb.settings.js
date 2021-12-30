a341dc4d5a03ea4c7fd2f9138d202e168ccab847
mhaszprunar <michaelhaszprunar@googlemail.com>
2021-12-30 13:00:16 +0100

    updates to settings.json

--- apps/pebblebat/pebbleb.settings.js
+++ apps/pebblebat/pebbleb.settings.js
@@ -2,7 +2,7 @@
     const SETTINGS_FILE = "pebbleb.json";
 
     // initialize with default settings...
-    let s = {'bg': '#0f0', 'color': 'Green', 'showBatteryNumber': "off", "refresh":15};
+    var s = {'bg': '#0f0', 'color': 'Green', 'showBatteryNumber': "off", "refresh":15};
 
     // ...and overwrite them with any saved values
     // This way saved values are preserved if a new version adds more settings
@@ -16,11 +16,12 @@
     function save() {
         settings = s;
         storage.write(SETTINGS_FILE, settings);
-        if (WIDGETS["myapp"]) WIDGETS["myapp"].reload()
     }
 
     var color_options = ['Green','Orange','Cyan','Purple','Red','Blue'];
     var bg_code = ['#0f0','#ff0','#0ff','#f0f','#f00','#00f'];
+    var number_options = ["on","off"];
+    var refresh_options = [15,30];
 
     E.showMenu({
         '': { 'title': 'Pebble + battery' },
@@ -36,23 +37,24 @@
             },
         },
         "number": {
-            value: !!settings.showBatteryNumber,
-            format: v => v ? v : ["off", "on"],
-            onChange: v => {
-                s.showBatteryNumber = v;
+            value: 0 | number_options.indexOf(s.showBatteryNumber),
+          min:0,max:1,
+            format: v => number_options[v],
+            onchange: v => {
+                s.showBatteryNumber = number_options[v];
                 save();
             }
 
         },
-        "refresh sec": {
-            value: !!settings.refresh,
-            format: v => v ? 15 : 30,
-            onChange: v => {
-                s.refresh = v;
+       "refresh sec": {
+            value: 0 | refresh_options.indexOf(s.refresh),
+         min:0,max:1,
+            format: v => refresh_options[v],
+            onchange: v => {
+                s.refresh = refresh_options[v];
                 save();
             }
 
-        },
-        
+        }
     });
-});
+})(load);
